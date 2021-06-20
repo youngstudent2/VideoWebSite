20a061d6489696610ad1d1d0b95bb958db85af7c
youngstudent2 <644699280@qq.com>
2021-06-20 16:34:39 +0800

    backend

---
+++ backend/src/main/java/cn/edu/nju/videowebsite/VideowebsiteApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.videowebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideowebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideowebsiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cn/edu/nju/videowebsite/VideowebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.videowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideowebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
